b17cbbec15a2e4887d9ea993ec44a2d548f29ecc
pt807 <pt807@naver.com>
2023-02-09 16:57:06 +0900

    ...

--- jblog03/docs/sql.sql
+++ jblog03/docs/sql.sql
@@ -6,9 +6,10 @@ select * from user;
 
 delete from user where id = 'fnvl';
 
-select * from blog where id = 'fnvl';
+select * from blog ;
+
 insert into blog values('testId', 'testTitle', 'testurl');
-update blog set profile = '/assets/images/spring-logo.jpg' where id = 'testId';
+update blog set title = '' and profile = '/assets/images/spring-logo.jpg' where id = 'testId';
 
 select * from category;
 insert into category values(null, 'category1', 'testId');
@@ -30,5 +31,22 @@ insert into post values(null, 'testPost2', 'testContents2', now(), 9);
 insert into post values(null, 'testPost3', 'testContents3', now(), 9);
 insert into post values(null, 'testPost4', 'testContents4', now(), 9);
 
-select no, title, contents, reg_date category_no as categoryNo from post where category_no = 8 order by no desc;
-select * from post where no = 5 and category_no = 8;
+select no, title, contents, reg_date, category_no as categoryNo from post where category_no = 8 order by no desc;
+select * from post where no = 1 and category_no = 8;
+
+select * 
+from category;
+
+select count(*)
+from post
+where category_no = 8;
+
+select a.no as no, a.name as name, count(b.no) as postCount
+			from category a, post b
+			where a.no = b.category_no and id = 'testId'	
+			group by b.category_no;
+            
+
+select no, name, (select count(*) from post where category_no = c.no) as postCount 
+from category c where id = 'testId';
+            
