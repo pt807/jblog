4fc48f49e364bec77a949331adb54a820e23ac55
pt807 <pt807@naver.com>
2023-02-14 19:13:05 +0900

    404err

--- jblog05/src/main/java/com/douzone/jblog/exception/GlobalExceptionHandler.java
+++ jblog05/src/main/java/com/douzone/jblog/exception/GlobalExceptionHandler.java
@@ -8,14 +8,20 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-	
+
 	private static final Log Logger = LogFactory.getLog(GlobalExceptionHandler.class);
-	
+
 	@ExceptionHandler(Exception.class)
 	public String handlerException(Exception e, Model model) {
+		// 1. 404 Error 처리
+		if (e instanceof NoHandlerFoundException) {
+			return "error/404";
+		}
+
 		// 1. 로깅(Logging)
 		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors));
@@ -23,11 +29,11 @@ public class GlobalExceptionHandler {
 		Logger.error(errors.toString());
 
 		// 2. 사과페이지( 3.정상종료)
-		
+
 		model.addAttribute("msg", "죄송합니다. 잠시 후 다시 시도해 주세요");
 		return "error/exception";
 	}
-	
+
 //	@ExceptionHandler(NoHandlerFoundException.class)
 //	@ResponseStatus(value = HttpStatus.NOT_FOUND)
 //	public String handle404(NoHandlerFoundException e, Model model) {
